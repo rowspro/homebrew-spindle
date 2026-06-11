e5b793c11539494109cc63641b4910768ac848fa
Lucas <lucasderivecourt@gmail.com>
2026-06-12 00:23:27 +0200

    Spindle 1.0.0 cask

---
+++ Casks/spindle.rb
@@ -0,0 +1,16 @@
+cask "spindle" do
+  arch arm: "AppleSilicon", intel: "Intel"
+
+  version "1.0.0"
+  sha256 arm:   "589d9f04c71caffea08c35d94e942e263918ab372c9b17fbedd67c839b4dc804",
+         intel: "c5e3ef0a9aaf9e21b5f00827471a139a05c0f982f8ad8f1440ff8506efd60e3b"
+
+  url "https://github.com/rowspro/Spindle/releases/download/v#{version}/Spindle-macOS-#{arch}.zip"
+  name "Spindle"
+  desc "Music library manager with an inbox quality gate and iPod sync"
+  homepage "https://github.com/rowspro/Spindle"
+
+  app "Spindle.app"
+
+  zap trash: "~/Library/Application Support/SeekDownloader"
+end
---
+++ README.md
@@ -0,0 +1,7 @@
+# Spindle Homebrew tap
+
+```
+brew install rowspro/spindle/spindle
+```
+
+That's it. See [rowspro/Spindle](https://github.com/rowspro/Spindle) for the app itself.
