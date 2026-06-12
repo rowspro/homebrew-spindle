a07219665b5f51d7367427399e4ef27d1b43a8f8
Lucas <lucasderivecourt@gmail.com>
2026-06-12 04:31:31 +0200

    Spindle 1.2.0

--- Casks/spindle.rb
+++ Casks/spindle.rb
@@ -1,9 +1,9 @@
 cask "spindle" do
-  arch arm: "AppleSilicon", intel: "5d085b7f827a7a6440caae9a682cdccdc6696b9240f17cd8074fd2358c855613"
+  arch arm: "AppleSilicon", intel: "dcb1df57957132d9959672bab6bca80d0930f7ab094e43aac6dd8eefbcd5d5de"
 
-  version "1.1.0"
-  sha256 arm:   "ebf892e1cb8e366806396694f238fbb9aa1d32ec91d0091bde4679f377322b33",
-         intel: "5d085b7f827a7a6440caae9a682cdccdc6696b9240f17cd8074fd2358c855613"
+  version "1.2.0"
+  sha256 arm:   "2688b254d42f75c34d49ac7034799be15ab126656b712d03874eb46916b741d8",
+         intel: "dcb1df57957132d9959672bab6bca80d0930f7ab094e43aac6dd8eefbcd5d5de"
 
   url "https://github.com/rowspro/Spindle/releases/download/v#{version}/Spindle-macOS-#{arch}.zip"
   name "Spindle"
